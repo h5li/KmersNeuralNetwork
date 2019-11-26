7521ab222c27aa3c2a407979f973441f6b6580ce
Han Li <h5li@ucsd.edu>
2019-11-26 15:47:31 -0800

    add eval for multiclass

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import math
+import sys
 
 def evaluate(net,data,levels,computing_device):
 
@@ -27,8 +28,11 @@ def evaluateMultiClass(net,data,levels,computing_device):
             x = torch.tensor(x).to(computing_device)
             output = net(x.float())
 
-            pred.append(output)
-    print(np.array(pred).shape)
+            pred.append(output.numpy)
+    pred = np.array(pred)
+    for i in range(levels.shape[1]):
+        sys.stdout.write("Cell Type: {} R2: {}".format(r2_score(levels[:,i],pred[:,i])))
+    sys.stdout.write("\n")
     #return math.sqrt(mean_squared_error(pred,levels)),r2_score(levels,pred)
 
 
