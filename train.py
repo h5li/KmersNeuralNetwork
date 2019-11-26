fbe1edba8530230c0ecc5b32a926e9999ec3b7a4
Han Li <h5li@ucsd.edu>
2019-11-25 23:39:29 -0800

    make the model working

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ import sys
 from model import *
 
 
-train_data = pd.read_csv('../data/Kmers6_counts_600bp.csv')
-train_methys = pd.read_csv('../data/Mouse_DMRs_counts_methylated.csv',header = None)
+train_data = pd.read_csv('../../data/Kmers6_counts_600bp.csv')
+train_methys = pd.read_csv('../../data/Mouse_DMRs_counts_methylated.csv',header = None)
 
 
 # Check if your system supports CUDA
@@ -50,16 +50,19 @@ net = Net(train_data.shape[1],1).to(computing_device)
 criterion = nn.MSELoss()
 
 #Instantiate the gradient descent optimizer - use Adam optimizer with default parameters
-optimizer = optim.Adam(net.parameters(),lr = 0.001)
+optimizer = optim.Adam(net.parameters(),lr = 0.0001)
 
 cell_type = 5
 X = train_data.as_matrix()
 train_X = X[train_indices]
 val_X = X[val_indices]
-Y = train_methys[cell_type]
+Y = np.array(train_methys[cell_type])
 
 train_Y = Y[train_indices]
 val_Y = Y[val_indices]
+
+
+print(train_X.shape,train_Y.shape,val_X.shape,val_Y.shape)
 
 for e in range(epochs):
 
@@ -67,32 +70,40 @@ for e in range(epochs):
     optimizer.zero_grad()
 
     train_loss = 0
-    for i,x in enumerate(train_X):
-        x = torch.tensor(x).to(computing_device)
-        y = torch.tensor(train_Y[i]).to(computing_device)
+    batch_count = 0
+    for i in range(len(train_X)//batch_size+1):
+        if i*batch_size >= len(train_X):continue
+        x = torch.tensor(train_X[i*batch_size:(i+1)*batch_size]).to(computing_device)
+        y = torch.tensor(train_Y[i*batch_size:(i+1)*batch_size]).view(-1,1).to(computing_device)
 
-        outputs = net(x)
-        loss = criterion(outputs,y)
+        outputs = net(x.float())
+        #train_pred.append(outputs.item())
+        #print(outputs.shape,y.float().shape)
+        loss = torch.sqrt(criterion(outputs,y.float()))
         train_loss += loss.item()
         loss.backward()
+        optimizer.step()
+        batch_count += 1
     
-    optimizer.step()
-
-    sys.stdout.write('\rEpoch {}, Train Loss: {}'.format(e,train_loss/len(train_X)))
+    sys.stdout.write('\rEpoch {}, Train Loss: {}'.format(e,train_loss/batch_count))
+    #sys.stdout.write('\rEpoch {}, Train R2: {}'.format(e,r2_score(train_Y,outputs)))
     print("\n")
 
     # Validate data
     val_loss = 0
-    for i,x in enumerate(val_X):
+    val_pred = []
+    batch_count = 0
+    for i in range(len(val_X)//batch_size+1):
 
         with torch.no_grad():
-            x = torch.tensor(x).to(computing_device)
-            y = torch.tensor(val_Y[i]).to(computing_device)
-
-            outputs = net(x)
-            loss = criterion(outputs,y)
+            x = torch.tensor(val_X[i*batch_size:(i+1)*batch_size]).to(computing_device)
+            y = torch.tensor(val_Y[i*batch_size:(i+1)*batch_size]).view(-1,1).to(computing_device)
+            outputs = net(x.float())
+            loss = torch.sqrt(criterion(outputs,y.float()))
             val_loss += loss.item()
-    sys.stdout.write('\rEpoch {}, Val Loss: {}'.format(e,val_loss/len(val_X)))
+            batch_count += 1
+    sys.stdout.write('\rEpoch {}, Val Loss: {}'.format(e,val_loss/batch_count))
+    #sys.stdout.write('\rEpoch {}, Val R2: {}'.format(e,r2_score(val_Y,val_pred)))
     print("\n")
 
 
