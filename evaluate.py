162ac9351188ec8c1e2081f2e39e8ae627b683e6
Han Li <h5li@ucsd.edu>
2019-11-26 21:07:46 -0800

    rewrite eval function

--- evaluate.py
+++ evaluate.py
@@ -39,18 +39,21 @@ def evaluateMultiClass(net,data,levels,computing_device):
 
 
 def evaluateMultiClassCNN(net,data,levels,computing_device):
+    batch_size = 64
 
     pred = []
-    for x in data:
+    for i in range(len(data)//batch_size+1):
+
         with torch.no_grad():
-            x = torch.tensor(x).view(1,4,600).to(computing_device)
-            output = net(x.float())
-            pred.append(output.cpu().numpy())
-    pred = np.array(pred)
-    score_sum = 0
+            x = torch.tensor(data[i*batch_size:(i+1)*batch_size]).to(computing_device)
+            #y = torch.tensor(val_Y[i*batch_size:(i+1)*batch_size]).to(computing_device)
+            outputs = net(x.float())
+            pred.append(outputs.numpy()[:,0,:])
+
+    pred = np.concatenate(pred,axis=1)
     for i in range(levels.shape[1]):
         sys.stdout.write("T:{} S:{:.4f} ".format(i,r2_score(levels[:,i],pred[:,0,i])))
-        score_sum += r2_score(levels[:,i],pred[:,0,i]) 
+        score_sum += r2_score(levels[:,i],pred[:,i]) 
     sys.stdout.write(str(max(0,score_sum)))
     sys.stdout.write("\n")
     #return math.sqrt(mean_squared_error(pred,levels)),r2_score(levels,pred)
